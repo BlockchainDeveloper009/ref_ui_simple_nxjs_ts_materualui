35b82f00262feec38845b8e8f19806644383183b
alpha@DESKTOP-6F6AEHR <blkcdev@gmail.com>
2023-05-25 15:38:32 -0500

    flush may-25-2023

--- petvets/pages/ContactUs.tsx
+++ petvets/pages/ContactUs.tsx
@@ -8,6 +8,10 @@ const ContactUs = () => {
                 {/* <img alt="Image of a suggested globe in flat gradient layers." src="./pet1.jpg"></img> */}
                 {/* <Image  width="800" height="600" src={pet1} alt=""></Image> */}
                 <span>I will fill some details for tseting and a good dev is working everyday towards the goal</span>
+                <h4>Hours of operation</h4>
+                <p>Monday - Friday (9am - 6pm)</p>
+                <p>Saturday (9am - 5pm)</p>
+                <p>Sunday (Closed)</p>
         </section>
       </main>
     );
---
+++ petvets/pages/Services/index.jsx
@@ -0,0 +1,26 @@
+const pageHeader = "About"
+const Services = () => {
+    return (
+        <main>
+            <h2>{pageHeader}</h2>
+            <section id="2">
+                {/* <img alt="Image of a suggested globe in flat gradient layers." src="./pet1.jpg"></img> */}
+                {/* <Image  width="800" height="600" src={pet1} alt=""></Image> */}
+                <div>
+                    <div class="image-wrapper">
+                        <a title="" href=""></a>
+                    </div>
+                    <div class="sc-5mwt0n-0 glewyr service-text">
+                        <h3 class="sc-1lhs05s-0 bQuPGT card-header" role="heading" aria-level="3"><a title="Microchipping" href="/services/microchipping" class=" sc-1ux5mjm-0 ivodzo" target="_self" data-analytics-type="cta-services-tile" data-analytics-value="Microchipping" data-analytics-variation="" aria-label="Microchipping">Microchipping</a></h3>
+                        <a title="Learn More" href="/services/microchipping" class=" sc-1ux5mjm-0 ivodzo" target="_self" data-analytics-type="cta-services-tile" data-analytics-value="Microchipping" data-analytics-variation="" aria-label="Microchipping">
+                            <div class="sc-wturor-0 kbsDvK card-rich-text">
+                                <p>Microchip identification is the most reliable way to reunite lost pets with their owners.</p></div></a>
+                        <p class="undefined sc-ovnq1p-0 gATzpR para-cta-wrapper"><a title="Learn More" href="/services/microchipping" class=" sc-1ux5mjm-0 ivodzo arrow" target="_self" tabindex="0" data-analytics-type="cta-services-tile" data-analytics-value="Microchipping" data-analytics-variation="" aria-label="Learn More">Learn More</a></p></div>
+                </div>
+
+            </section>
+        </main>
+    );
+};
+
+export default Services;
